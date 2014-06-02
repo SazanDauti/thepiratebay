1b75af5d3ff8cfd7f45deb7250d68befd2b2b05e
Sazan Dauti <sazandauti@sazandauti.com>
2014-06-01 22:07:58 -0400

    Create example.php

---
+++ example.php
@@ -0,0 +1,25 @@
+<?php
+
+require("thepiratebay.class.php");
+
+$query = "example";
+$page  = 1;
+
+$tpb    = new ThePirateBay;
+$search = $tpb->searchTorrents($query, $page);
+
+foreach ($search as $info) {
+    $name               = $info['name'];
+    $link               = $info['link'];
+    $main_category      = $info['main_category'];
+    $secondary_category = $info['secondary_category'];
+    $seeders            = $info['seeders'];
+    $leechers           = $info['leechers'];
+    $description        = $info['description'];
+    $magnet_link        = $info['magnet_link'];
+    
+    echo "Name: " . $name . "<br />Link: " . $link . "<br />Main Category: " . $main_category . "<br />Secondary Category: " . $secondary_category . "<br />Seeders: " . $seeders . "<br />Leechers: " . $leechers . "<br />Description: " . $description . "<br />Magnet Link: " . $magnet_link . "<br /><br />---------------<br /><br />";
+    
+}
+
+?>
